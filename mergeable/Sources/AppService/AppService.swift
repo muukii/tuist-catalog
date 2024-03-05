ceddb5c4d2f4b29479457106592e879b4b17dbd1
Muukii <muukii.app@gmail.com>
2024-03-05 13:19:57 +0900

    Update

--- mergeable/Sources/AppService/AppService.swift
+++ mergeable/Sources/AppService/AppService.swift
@@ -3,7 +3,23 @@ import Verge
 
 public enum AppService {
 
+  struct State: StateType {
+    var count: Int = 0
+  }
+
   public static func run() {
-    print("Hello, I'm AppService")
+
+    let store = Store<State, Never>(initialState: .init())
+
+    store.sinkState { state in
+      print("State updated: \(state)")
+    }
+    .storeWhileSourceActive()
+
+    print("Hello, I'm AppService, \(store)")
+
+    store.commit {
+      $0.count += 1
+    }
   }
 }
